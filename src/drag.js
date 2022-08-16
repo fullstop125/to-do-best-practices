810ad53f26b7f82ad2eb7b8e8543c7ee3ca2a4d1
fullstop125 <hassanmoses125@gmail.com>
2022-08-16 16:25:59 +0300

    fix all required changes

--- src/drag.js
+++ src/drag.js
@@ -2,11 +2,12 @@
  * @param dragElement - the element that is being dragged
  * @param currentTask - the task that is currently in the column
  */
-function dropElements(dragElement, currentTask) {
+  let dropElements = (dragElement, currentTask) => {
   const pastTask = currentTask;
   const pastCont = currentTask.parentNode;
   const newTask = dragElement;
-  const newCont = dragElement.parentNode;
+  const newCont = dragElement.parentNode; 
+
   pastCont.appendChild(newTask);
   newCont.appendChild(pastTask);
 }
